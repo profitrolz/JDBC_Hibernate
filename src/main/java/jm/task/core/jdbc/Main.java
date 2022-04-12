3ef7cd21dc4ee3b336f3146b0a75dc2c1463d6e4
Lexa <a.pisarenko9001@gmail.com>
2022-04-12 12:47:07 +0700

    Main method realisation User toString override

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,21 +1,29 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.service.UserService;
 import jm.task.core.jdbc.service.UserServiceImpl;
 import jm.task.core.jdbc.util.Util;
 
 import javax.sql.DataSource;
 import java.sql.SQLException;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
-        DataSource dataSource = Util.getDataSource();
-        dataSource.getConnection();
-        System.out.println("Ok!");
 
         UserService service = new UserServiceImpl();
-        service.saveUser("Vasya", "Pupkin", (byte)18);
-        service.removeUserById(1L);
+        service.createUsersTable();
+
+        service.saveUser("Johnny", "Sins", (byte)42);
+        service.saveUser("Patrik", "Betta", (byte)36);
+        service.saveUser("Li", "Keiran", (byte)35);
+        service.saveUser("James", "Din", (byte)27);
+
+        service.getAllUsers().forEach(System.out::println);
+
+        service.cleanUsersTable();
+        service.dropUsersTable();
 
     }
 }
